6cd8d71a9b7ab7143091f728bd037345e117de8d
minkyu <97155555+minkku@users.noreply.github.com>
2024-02-29 15:21:25 +0900

    modified init setting for board govFramework

---
+++ basicEGovFrame/src/main/java/controller/MainController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	@GetMapping("/main.do")
+	public String mainPage() {
+		return "main";
+	}
+}
